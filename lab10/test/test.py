4d5708bb5b5fc0c176f5fe3f8c9f88791581922d
icansay <wo.icansay@gmail.com>
2023-04-23 22:52:56 +0600

    testlab10

---
+++ lab10/test/test.py
@@ -0,0 +1,15 @@
+# import psycopg2
+
+# conn = psycopg2.connect("dbname=suppliers user=postgres password=khan2281337")
+# cur = conn.cursor()
+        
+# # execute a statement
+# print('PostgreSQL database version:')
+# cur.execute('SELECT version()')
+
+# # display the PostgreSQL database server version
+# db_version = cur.fetchone()
+# print(db_version)
+       
+# # close the communication with the PostgreSQL
+# cur.close()
